2ef855c793e58295b5715526316c10ff0001dae5
Jonathan Bogdanovicz <jhonnybail@gmail.com>
2020-05-25 15:53:15 -0300

    feat(auth): Verify if collaborator is current employed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import localStorage from 'react-native-local-storage';
 import { NavigationActions } from 'react-navigation'
+import { API_URL } from 'react-native-dotenv';
 
 class HomeScreen extends React.Component {
 
@@ -16,8 +17,24 @@ class HomeScreen extends React.Component {
     async componentDidMount() {
         const isLogged = await this.isLogged()
 
-        if (isLogged) this.props.dispatch(NavigationActions.navigate({ routeName: 'resume' }))
-        else this.props.dispatch(NavigationActions.navigate({ routeName: 'document' }))
+        if (isLogged){
+            const json = await localStorage.get('user');
+            const collaborator = json ? JSON.parse(json) : null;
+            
+            if(collaborator) {
+                const isValid = await this.isValidCollaborator(collaborator)
+
+                if(isValid) this.props.dispatch(NavigationActions.navigate({ routeName: 'resume' }))
+                else this.props.dispatch(NavigationActions.navigate({ routeName: 'document' }))
+            }
+        }else this.props.dispatch(NavigationActions.navigate({ routeName: 'document' }))
+    }
+
+    isValidCollaborator = async collaborator => {
+        const response = await fetch(`${API_URL}colaborador/${collaborator.id}`);
+        const json = await response.json()
+
+        return json.trabajo.fechasalida === undefined;
     }
 
     render () {
